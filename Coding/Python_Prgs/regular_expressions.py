6ef2cfa50bd98be6a79390e278d4aa45fe0c9e98
Phantom Boy <586pboy@gmail.com>
2019-04-23 16:22:16 +0530

    commit for regular_expr.py

--- Coding/Python_Prgs/regular_expressions.py
+++ Coding/Python_Prgs/regular_expressions.py
@@ -15,11 +15,13 @@ example_string = """
                 https://www.youtube.com/watch?v=K8L6KVGG-7o
                  """
 
-# pattern = re.compile(r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z]+')
-# Match any type of emails
 
-pattern = re.compile(r'(http|https)?(://)?[a-zA-Z]+\.[a-zA-Z0-9]+\.[a-z]+[/a-zA-Z0-9?=+-_]*')
+# Match any type of emails
+pattern = re.compile(r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z]+')
+
+
 # Match any type of URL's
+pattern = re.compile(r'(http|https)?(://)?[a-zA-Z]+\.[a-zA-Z0-9]+\.[a-z]+[/a-zA-Z0-9?=+-_]*')
 
 matches = pattern.finditer(example_string)
 
