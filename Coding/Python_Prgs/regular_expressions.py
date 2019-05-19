f8165430c1d0ce2c241fbb29fe392ddd658f29b0
Phantom Boy <586pboy@gmail.com>
2019-05-19 23:58:54 +0530

    commit for some changes

--- Coding/Python_Prgs/regular_expressions.py
+++ Coding/Python_Prgs/regular_expressions.py
@@ -5,6 +5,9 @@ example_string = """
                 Fhewfb#186
                 Juven5876@gmail.com
                 HDFKHG287#
+                Phantom#123
+                Dyn_586
+                ImIn54n3
                 h.kdjfhkdf@gmail.com
                 DFj_545@gmail.com
                 Fdhfh_4551@yahoo.com
@@ -23,12 +26,18 @@ email_pattern = re.compile(r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z]+')
 # Match any type of URL's
 url_pattern = re.compile(r'(http|https)?(://)?[a-zA-Z]+\.[a-zA-Z0-9]+\.[a-z]+[/a-zA-Z0-9?=+-_]*')
 
+# Matching Passwords
+pass_pattern = re.compile(r'[A-Z]+[a-zA-Z0-9!@#$%^&*()_+]+[!@#$%^&*()_+]+[a-zA-Z0-9]+')
+
 match1 = email_pattern.finditer(example_string)
 
 match2 = url_pattern.finditer(example_string)
 
-for match in match1:
-    print(match)
+match3 = pass_pattern.finditer(example_string)
 
-for match in match2:
-    print(match)
+# for match in match3:
+#     print(match)
+print(pass_pattern.match("ph@ntom#123"))
+
+# for match in match2:
+#     print(match)
---
+++ Coding/Python_Prgs/sys_module.py
@@ -0,0 +1,5 @@
+import sys
+
+if len(sys.argv) == 3:
+    a, b = sys.argv[1], sys.argv[2]
+    print("Addition is :", int(a) + int(b))
