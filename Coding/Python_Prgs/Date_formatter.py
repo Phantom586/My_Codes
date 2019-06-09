a4e8230741d54d3738a50d03061f8c2ff4bfff10
Phantom Boy <586pboy@gmail.com>
2019-06-09 14:45:59 +0530

    commit for changes in date_formatter

--- Coding/Python_Prgs/Date_formatter.py
+++ Coding/Python_Prgs/Date_formatter.py
@@ -1,6 +1,7 @@
 # Takes Differently formatted Dates and prints out it in a Standard Format.
 import re
 
+# Regular Expression for Matching Date Formats Like : DD(-/)MM(-/)YYYY or YYYY(-/)MM(-/)DD
 date = re.compile(r'''(
         ( 
         ((\d|\d{2})+(-|/)+(\d|\d{2})+(-|/)+(\d{4})+) | ((\d{4})+(-|/)+(\d|\d{2})+(-|/)+(\d|\d{2})+)
@@ -8,9 +9,11 @@ date = re.compile(r'''(
         )''', re.VERBOSE)
 
 str = input("Enter the Date or String of Dates in [DD-MM-YYYY] or [YYYY-MM-DD].\n-->")
-inp_dates = []
-oup_dates = []
-# print(date.match(str))
+inp_dates = []  # list for dates matched from the regular expression.
+oup_dates = []  # list for the dates Reformatted in Standard  Format.
+
+# loop to find the matches in string 'str' and then split them according to their respective delimiters '-' or '/'
+# then append into the 'inp_dates' list.
 for g in date.finditer(str):
     item = g.group()
     if '-' in item:
@@ -19,11 +22,13 @@ for g in date.finditer(str):
         d = item.split('/')
     inp_dates.append(d)
 
+# loop to change the Dates to standard Format.
 for d in inp_dates:
     if len(d[2]) == 4:
         d[0], d[1], d[2] = d[2], d[1], d[0]
     oup_dates.append('-'.join(d))
 
+# Generating a string of Standard Formatted Dates.
 str = ''
 for i in oup_dates:
     str += i + '\n' 
